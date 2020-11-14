5b42adc93d51eadab95b7ae94ead0ec160dcb481
Colson Scott <colsonscott53@gmail.com>
2020-11-14 20:28:28 +0000

    Readded secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '42&$lr5tlk9pu(s=e^qd*1n5r$*94!^_7lec(g051!c4+grvgg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
